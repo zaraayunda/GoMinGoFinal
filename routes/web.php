e9db2db04802d6a8a1c0cb83354202e7545842f8
Zara Ayunda <zaraayunda1402@gmail.com>
2025-11-01 09:28:33 +0700

    template login dan regis

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('layout.dashboard');
+});
+Route::get('/login', function () {
+    return view('layout.login');
+});
+Route::get('/registrasi', function () {
+    return view('layout.registrasi');
+});
